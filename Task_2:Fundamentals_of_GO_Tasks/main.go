b14fc921b9e180007f5adccde5466e29aa552c3a
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-08-05 23:11:13 +0300

    Finish(task--2): Finish word count and isPalindrome functions

---
+++ Task_2:Fundamentals_of_GO_Tasks/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"strings"
+)
+
+func freqCount(s string) map[string]int {
+	splitted := strings.Split(s, " ")
+	freq := make(map[string]int)
+
+	for _, word := range splitted {
+		word = strings.ToLower(word)
+		freq[word] += 1
+	}
+	return freq
+}
+
+func isPalindrome(s string) bool {
+	left := 0
+	right := len(s) - 1
+
+	for left < right {
+		if s[left] != s[right] {
+			return false
+		}
+		left++
+		right--
+	}
+
+	return true
+}
+
+func main() {
+
+}
